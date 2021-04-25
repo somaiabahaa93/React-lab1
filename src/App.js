13e2fe5f97d0dd06f7321ffcb20df58f78434624
somaiabahaa93 <somaianaggar93@gmail.com>
2021-04-25 20:00:52 +0200

    first lab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import React  from 'react';
 
-function App() {
+class App extends React.Component {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+
+     welcome to our counter
+     <Counter/>
     </div>
   );
+  
+}
+}
+
+class Counter extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      count:0
+    }
+  }
+  increase=()=>{
+    this.setState({count:this.state.count+1})
+  }
+  decrease=()=>{
+    this.setState({count:this.state.count-1})
+  }
+  render(){
+    return(
+      <div>
+<div>{this.state.count}</div>
+<button onClick={this.increase}>Increase</button>
+<button onClick={this.decrease}>Decrease</button>
+{/* <div>iam usin {this.props.brand}</div> */}
+
+</div>
+    );
+  }
 }
 
 export default App;
